df1ac408427a8dafd029a5658ec56419e465256d
Alex Johnson <alex.johnson293@gmail.com>
2022-12-06 15:03:21 -0800

    add Knight logic for legal moves

--- Chessboard/Models/Board.cs
+++ Chessboard/Models/Board.cs
@@ -22,5 +22,47 @@ namespace Chessboard.Models
         }
       }
     }
+  public void NextLegalMoves(Cell currentCell, string piece)
+  {
+    // clear all previous moves
+    for (int i = 0; i < Size; i++)
+      {
+        for (int j = 0; j < Size; j++)
+        {
+          boardGrid[i,j].LegalMove = false;
+          boardGrid[i,j].Occupied = false;
+        }
+      }
+    // find all legal moves & mark legal
+    
+    switch(piece)
+    {
+      case :"Knight":
+        boardGrid[currentCell.RowNumber + 2, currentCell.ColNumber + 1].LegalMove = true;
+        boardGrid[currentCell.RowNumber + 2, currentCell.ColNumber - 1].LegalMove = true;
+        boardGrid[currentCell.RowNumber - 2, currentCell.ColNumber + 1].LegalMove = true;
+        boardGrid[currentCell.RowNumber - 2, currentCell.ColNumber - 1].LegalMove = true;
+        boardGrid[currentCell.RowNumber + 1, currentCell.ColNumber + 2].LegalMove = true;
+        boardGrid[currentCell.RowNumber + 1, currentCell.ColNumber - 2].LegalMove = true;
+        boardGrid[currentCell.RowNumber - 1, currentCell.ColNumber + 2].LegalMove = true;
+        boardGrid[currentCell.RowNumber - 1, currentCell.ColNumber - 2].LegalMove = true;
+        break;
+
+      case "King":
+        break;
+
+      case "Queen":
+        break;
+
+      case "Rook":
+        break;
+      
+      case "Bishop":
+        break;
+
+      case "Pawn":
+        break;
+    }
+  }
   }
 }
